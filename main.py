b8a26cacd05a8737c0f965d5ab6840e7b78c64a0
Pouyamaleki <pouyamaleki85@gmail.com>
2026-07-28 16:50:35 +0330

    feat: add Covariance_matrix function to the project

--- main.py
+++ main.py
@@ -42,12 +42,33 @@ def Center_data(X, mean_vector):
         X_centered: centerd data matrix
         mean_vector: mean of rach feature
     '''
-    
+
     # subtract the mean of each sample
     X_centered = X - mean_vector
-    
+
     # verify the centring proccess
     column_mean = numpy.mean(X_centered, 0)
-    
+
     return X_centered
-    
+
+
+def Covariance_matrix(X_centered):
+    '''
+    Step 3: Compute the covariance matrix
+    Input:
+        X_centered: centered data matrix (m * n)
+    Returns:
+        C: Covariance matrix (m * n)
+        is_symmetric: is covariance matrix symmetric or no
+    '''
+
+    #
+    m, n = X_centered.shape
+
+    # Compute the covariance matrix
+    C = (1 / (m-1)) * numpy.dot(X_centered.t, X_centered)
+
+    # check if the covariance matrix is symmetric or no
+    is_symmetric = numpy.allclose(C, C.t)
+
+    return C, is_symmetric
